d7a1fa188a638d71f950e48c43f69d075881fca6
Jens Jaeger <jaegerj204@googlemail.com>
2017-04-19 10:12:58 +0200

    show flat tree

--- src/components/Tree/Component.tsx
+++ src/components/Tree/Component.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import {AppState} from  "../../typings"
-import {SwitchExpandAction, TreeActions, TreeWidget, TreeList, TreeItem} from "./typings"
+import {SwitchExpandAction, TreeActions, TreeWidget, TreeItem} from "./typings"
 import {connect} from "react-redux";
 require("./style.css");
 const treeStyle = "tree";
@@ -15,20 +15,14 @@ const showItem = (item: TreeItem, switchExpand: SwitchExpandAction): JSX.Element
             checked={item.isExpanded}
             onChange={eventHandler}/>
         <span>{item.name}</span>
-        {item.children && item.isExpanded && (<ul>{listChildren(item.children, switchExpand)}</ul>)}
     </div>;
 } 
-
-const listChildren = (children: TreeList, switchExpand: SwitchExpandAction): JSX.Element[] => {
-    return children.map((entry) => <li key={entry.id}>{showItem(entry, switchExpand)}</li>);
-}
-    
 
 const Tree = ({title, data, switchExpand}: TreeWidget & TreeActions) =>{
     return <div>
         <h2>{title}</h2>
         <div className="tree">
-            <ul>{listChildren(data, switchExpand)}</ul>
+            <ul>{data.map(e => showItem(e, switchExpand))}</ul>
         </div>
     </div>;
 } 
