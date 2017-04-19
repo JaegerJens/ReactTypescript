e58fcf088cc03d0c60160b2fe1f3595e1b1a29d6
Jens Jaeger <jaegerj204@googlemail.com>
2017-04-19 13:14:48 +0200

    refactor tree util module and explicit ts lib declaration

--- src/components/Tree/util.tsx
+++ src/components/Tree/util.tsx
@@ -1,12 +1,41 @@
 import {TreeWidget, TreeItem} from "./typings"
 
-const util = {
-    getChildren: (data: TreeItem[], parentId: number): TreeItem[] => {
-        return data.filter(elem => elem.parentId === parentId);
-    },
-    getRoot: (data: TreeItem[]): TreeItem => {
-        return data.filter(elem => !elem.parentId)[0];
+const getRoot = (data: TreeItem[]): TreeItem => {
+    if (!data || data.length < 1)
+        return null;
+    let first = data[0];
+    if (!first.parentId)
+        return first;
+    
+    let entriesWithoutParents = data.filter(elem => !elem.parentId);
+    if (entriesWithoutParents.length == 1)
+        return entriesWithoutParents[0];
+    throw "too few or too many roots";
+}
+
+const getChildrenWithFullsearch = (data: TreeItem[], parentId: number): TreeItem[] => {
+    return data.filter(elem => elem.parentId === parentId);
+};
+
+const computeParentIndex = (data: TreeItem[]) => {
+    // map TreeItem.id -> array index
+    let map = new Map();
+    for(let i=0; i<data.length; i++) {
+        let item = data[i];
+        map.set(item.id, i);
+
+        if (!item.parentId)
+            continue;
+
+        let parentIndex = map.get(item.parentId);
+        item.parentIndex = parentIndex;
     }
+}
+
+const util = {
+    recalcParentIndex: computeParentIndex,
+    getChildren: getChildrenWithFullsearch,
+    getRoot: getRoot
 };
 
 export default util;
