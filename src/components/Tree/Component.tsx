9075f1ec73c9bba152b58b992de7ac4658e3000f
Jens Jaeger <jaegerj204@googlemail.com>
2017-04-19 10:34:16 +0200

    show expanded children of flat tree

--- src/components/Tree/Component.tsx
+++ src/components/Tree/Component.tsx
@@ -5,9 +5,20 @@ import {connect} from "react-redux";
 require("./style.css");
 const treeStyle = "tree";
 
-const showItem = (item: TreeItem, switchExpand: SwitchExpandAction): JSX.Element =>{
+const getChildren = (data: TreeItem[], parentId: number): TreeItem[] => {
+    return data.filter(elem => elem.parent === parentId);
+}
+
+const getRoot = (data: TreeItem[]): TreeItem => {
+    return data.filter(elem => !elem.parent)[0];
+}
+
+const showItem = (data: TreeItem[], item: TreeItem, switchExpand: SwitchExpandAction): JSX.Element =>{
 
     const eventHandler = (event:any) => switchExpand(item, !item.isExpanded);
+
+    let children = getChildren(data, item.id);
+    let showChildren: boolean = !!children && item.isExpanded;
 
     return <div>
         <input type="checkbox"
@@ -15,14 +26,16 @@ const showItem = (item: TreeItem, switchExpand: SwitchExpandAction): JSX.Element
             checked={item.isExpanded}
             onChange={eventHandler}/>
         <span>{item.name}</span>
+        {showChildren && children.map(child => (<ul>{showItem(data, child, switchExpand)}</ul>))}
     </div>;
 } 
 
 const Tree = ({title, data, switchExpand}: TreeWidget & TreeActions) =>{
+    let root = getRoot(data);
     return <div>
         <h2>{title}</h2>
         <div className="tree">
-            <ul>{data.map(e => showItem(e, switchExpand))}</ul>
+            <ul>{showItem(data, root, switchExpand)}</ul>
         </div>
     </div>;
 } 
