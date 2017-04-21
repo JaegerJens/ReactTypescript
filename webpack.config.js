df4449f19d16432d90f8f21e05ec0075d6b170ac
Jens Jaeger <jaegerj204@googlemail.com>
2017-04-21 09:37:45 +0200

    configure webpack to build external css file

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
     entry: "./src/index.tsx",
     output: {
@@ -12,10 +14,14 @@ module.exports = {
     module: {
         rules: [
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-            { test: /\.css$/, use: [ "style-loader", "css-loader" ] },
+            { test: /\.css$/, use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [ 'css-loader' ]
+            }) },
             { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
             { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
         ]
-    }
+    },
+    plugins: [ new ExtractTextPlugin({filename: '[name].css'})]
 };
